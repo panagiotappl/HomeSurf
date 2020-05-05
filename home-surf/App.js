ca0bb83ab0b7eb960003f09d5a65f87e69befff7
giota <panagiotappl@gmail.com>
2020-05-05 18:45:22 +0200

    Update dependecies and Home page

--- home-surf/App.js
+++ home-surf/App.js
@@ -1,19 +1,8 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+import Home from './screens/Home'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>HomeSurf</Text>
-    </View>
-  );
+  return <Home />
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
